a5a9cc01dddff96e3385889a1192c6fb6045cc39
castlele <castlelecs@gmail.com>
2025-01-01 15:20:01 +0300

    [feat]: add setup function

--- lua/build.lua
+++ lua/build.lua
@@ -9,18 +9,47 @@ local config = require("telescope.config").values
 local FM = require("cluautils.file_manager")
 require("cluautils.string_utils")
 
-local M = {}
-local buildFile = "build.lua"
+---@class build.TelescopeOptions
+---@field enabled boolean
+---@field keymap string
+
+---@class build.BuildOptions
+---@field buildFileName string?
+---@field telescope build.TelescopeOptions?
+
+---@class build.BuildModule
+---@field opts build.BuildOptions
+local M = {
+   opts = {
+      buildFileName = "build.lua",
+      telescope = {
+         enabled = pcall(require, "telescope"),
+         keymap = "<leader>B",
+      },
+   },
+}
+
+---@return string
+function M.getBuildFile()
+   return M.opts.buildFileName
+end
+
+---@return boolean
+function M.isTelescopeEnabled()
+   local opts = M.opts
+
+   return opts.telescope.enabled
+end
 
 local function editBuildFile()
    vim.cmd("vsplit")
-   vim.cmd("edit " .. buildFile)
+   vim.cmd("edit " .. M.getBuildFile())
 end
 
 local defaultCommandsAsStr = {
    edit = [[
 vim.cmd("vsplit")
-vim.cmd("edit " .. buildFile)
+vim.cmd("edit " .. M.getBuildFile())
    ]],
    generate = "Generating build.lua in the root folder :)",
 }
@@ -28,6 +57,7 @@ vim.cmd("edit " .. buildFile)
 local defaultCommands = {
    edit = editBuildFile,
    generate = function()
+      local buildFile = M.getBuildFile()
       if FM.is_file_exists(buildFile) then
          vim.notify(
             "Your build.lua file is already generated. You can edit it with `:Build edit` command",
@@ -49,7 +79,7 @@ conf = {
 
 ---@return boolean
 local function reloadConfigurationFile()
-   local fun, _ = loadfile(buildFile)
+   local fun, _ = loadfile(M.getBuildFile())
 
    if fun then
       pcall(fun)
@@ -120,6 +150,54 @@ function M.buildInTerm(command)
       :toggle()
 end
 
+local function searchBuild()
+   local opts = {
+      cwd = vim.uv.cwd(),
+   }
+
+   local finder = finders.new_table {
+      results = M.completion(),
+      entry_maker = makeEntry.gen_from_string(opts),
+      cwd = opts.cwd,
+   }
+
+   pickers
+      .new(opts, {
+         debounce = 100,
+         prompt_title = "Build Commands",
+         finder = finder,
+         previewer = previewers.new_buffer_previewer {
+            define_preview = function(self, entry)
+               local command = M.asString(entry[1])
+
+               if not command then
+                  return
+               end
+
+               local formattedCommand = string.split(command, "\n")
+
+               vim.api.nvim_buf_set_lines(
+                  self.state.bufnr,
+                  0,
+                  -1,
+                  false,
+                  formattedCommand
+               )
+            end,
+         },
+         sorter = config.generic_sorter(opts),
+         attach_mappings = function(prompt_bufnr, _)
+            actions.select_default:replace(function()
+               actions.close(prompt_bufnr)
+               local selection = actionState.get_selected_entry()
+               M.buildInTerm(selection[1])
+            end)
+            return true
+         end,
+      })
+      :find()
+end
+
 ---@param command string
 ---@return (fun(): string)?
 function M.build(command)
@@ -173,64 +251,43 @@ function M.completion(completion)
    return filteredConf
 end
 
-local function searchBuild()
-   local opts = {
-      cwd = vim.uv.cwd(),
-   }
-
-   local finder = finders.new_table {
-      results = M.completion(),
-      entry_maker = makeEntry.gen_from_string(opts),
-      cwd = opts.cwd,
-   }
-
-   pickers
-      .new(opts, {
-         debounce = 100,
-         prompt_title = "Build Commands",
-         finder = finder,
-         previewer = previewers.new_buffer_previewer {
-            define_preview = function(self, entry)
-               local command = M.asString(entry[1])
-
-               if not command then
-                  return
-               end
-
-               local formattedCommand = string.split(command, "\n")
-
-               vim.api.nvim_buf_set_lines(
-                  self.state.bufnr,
-                  0,
-                  -1,
-                  false,
-                  formattedCommand
-               )
-            end,
-         },
-         sorter = config.generic_sorter(opts),
-         attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-               actions.close(prompt_bufnr)
-               local selection = actionState.get_selected_entry()
-               M.buildInTerm(selection[1])
-            end)
-            return true
-         end,
-      })
-      :find()
-end
-
 local function buildCommandBinding(args)
    M.buildInTerm(args.args)
 end
 
-vim.api.nvim_create_user_command("Build", buildCommandBinding, {
-   desc = "build.nvim plugin. More here: https://github.com/castlele/build.nvim",
-   complete = M.completion,
-   nargs = 1,
-})
+---@param opts build.BuildOptions?
+function M.setup(opts)
+   if opts then
+      M.opts.buildFileName = opts.buildFileName or M.opts.buildFileName
 
-vim.keymap.set("n", "<leader>B", searchBuild, { noremap = true, silent = true })
+      local t = opts.telescope
+
+      if t then
+         M.opts.telescope.enabled = t.enabled or M.opts.telescope.enabled
+         M.opts.telescope.keymap = t.keymap or M.opts.telescope.keymap
+      end
+   end
+
+   vim.api.nvim_create_user_command("Build", buildCommandBinding, {
+      desc = "build.nvim plugin. More here: https://github.com/castlele/build.nvim",
+      complete = M.completion,
+      nargs = 1,
+   })
+
+   M.enableTelescopeIntegration()
+end
+
+function M.enableTelescopeIntegration()
+   if not M.isTelescopeEnabled() then
+      return
+   end
+
+   vim.keymap.set(
+      "n",
+      M.opts.telescope.keymap,
+      searchBuild,
+      { noremap = true, silent = true }
+   )
+end
 
 return M
